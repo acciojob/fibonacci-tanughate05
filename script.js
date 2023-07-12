afd75c1a311817ef5cd11411423aeae095ef777d
tanughate05 <93458822+tanughate05@users.noreply.github.com>
2023-07-12 12:39:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 function fibonacci(num) {
 //
-	if (num == 1)
-        return 0;
-    if (num == 2)
-        return 1;
-    return fibonacci(num - 1) + fibonacci(num - 2);
+	if(num < 2)
+		return num
+	return fibonacci(num-1) + fibonacci(num-2)
+	
 }
 
 module.exports = fibonacci;
